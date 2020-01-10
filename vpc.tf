bf60d490114525c96960691c6d91f380d4c6fc6d
intusurg-iw <irene.wang@intusurg.com>
2020-01-10 00:37:57 +0000

    update

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+//--------------------------------------------------------------------
+// Modules
+module "vpc" {
+  source  = "app.terraform.io/intusurg-ops/vpc/aws"
+  version = "2.21.0"
+
+  database_subnet_assign_ipv6_address_on_creation = "false"
+  elasticache_subnet_assign_ipv6_address_on_creation = "false"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+  intra_subnet_assign_ipv6_address_on_creation = "false"
+  private_subnet_assign_ipv6_address_on_creation = "false"
+  public_subnet_assign_ipv6_address_on_creation = "false"
+  redshift_subnet_assign_ipv6_address_on_creation = "false"
+  cidr = "10.0.0.0/16"  
+  region = "us-west-2"
+}
